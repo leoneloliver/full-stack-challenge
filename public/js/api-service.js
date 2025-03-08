1a4f765ee8dc1835852ac7a48bc7d6ee27ac4982
Leonel Oliveira <leoneloliver@gmail.com>
2025-03-07 20:21:34 -0500

    update relative path api

--- public/js/api-service.js
+++ public/js/api-service.js
@@ -1,11 +1,8 @@
 // api-service.js
-const baseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://full-stack-challenge-3ljc.onrender.com'
-  : 'http://127.0.0.1:8000';
-
 const ApiService = {
   fetchJobListings() {
-    return fetch(`${baseUrl}/joblists`)
+    // Use a relative URL - this will work in both environments
+    return fetch('/joblists')
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
